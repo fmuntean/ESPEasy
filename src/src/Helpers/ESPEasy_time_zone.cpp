60907e55da27535804536888e3c8169e53fd89b2
Florin <fmuntean@gmail.com>
2020-05-22 23:03:04 -0400

    Set default Timezone to EST

--- src/src/Helpers/ESPEasy_time_zone.cpp
+++ src/src/Helpers/ESPEasy_time_zone.cpp
@@ -17,13 +17,21 @@
 
 
 void ESPEasy_time_zone::getDefaultDst_flash_values(uint16_t& start, uint16_t& end) {
-  // DST start: Last Sunday March    2am => 3am
-  // DST end:   Last Sunday October  3am => 2am
-  TimeChangeRule CEST(TimeChangeRule::Last, TimeChangeRule::Sun, TimeChangeRule::Mar, 2, Settings.TimeZone); // Summer Time
-  TimeChangeRule CET(TimeChangeRule::Last, TimeChangeRule::Sun, TimeChangeRule::Oct, 3, Settings.TimeZone);  // Standard Time
 
-  start = CEST.toFlashStoredValue();
-  end   = CET.toFlashStoredValue();
+  //MFD: set default timezone to EST
+  // US Eastern Time Zone (New York)
+  TimeChangeRule usEDT(TimeChangeRule::Second, TimeChangeRule::Sun, TimeChangeRule::Mar, 2, -240);  // Eastern Daylight Time = UTC - 4 hours
+  TimeChangeRule usEST(TimeChangeRule::First, TimeChangeRule::Sun, TimeChangeRule::Nov, 2, -300);   // Eastern Standard Time = UTC - 5 hours
+  
+  // DST start: Last Sunday March    2am => 3am
+  // DST end: 	Last Sunday October  3am => 2am
+  //TimeChangeRule CEST(Last, Sun, Mar, 2, Settings.TimeZone); // Summer Time
+  //TimeChangeRule CET(Last, Sun, Oct, 3, Settings.TimeZone);  // Standard Time
+  //start = CEST.toFlashStoredValue();
+  //end = CET.toFlashStoredValue();
+
+  start = usEDT.toFlashStoredValue();
+  end = usEST.toFlashStoredValue();
 }
 
 void ESPEasy_time_zone::applyTimeZone(uint32_t curTime) {
@@ -33,7 +41,7 @@ void ESPEasy_time_zone::applyTimeZone(uint32_t curTime) {
 
   for (int i = 0; i < 2; ++i) {
     TimeChangeRule start(tmpStart, Settings.TimeZone + dst_offset); // Summer Time
-    TimeChangeRule end(tmpEnd, Settings.TimeZone);                  // Standard Time
+    TimeChangeRule end(tmpEnd, Settings.TimeZone);               // Standard Time
 
     if (start.isValid() && end.isValid()) {
       setTimeZone(start, end, curTime);
@@ -90,20 +98,20 @@ void ESPEasy_time_zone::logTimeZoneInfo() {
 // *----------------------------------------------------------------------*/
 uint32_t ESPEasy_time_zone::calcTimeChangeForRule(const TimeChangeRule& r, int yr)
 {
-  uint8_t m = r.month; // temp copies of r.month and r.week
-  uint8_t w = r.week;
+    uint8_t m = r.month;     // temp copies of r.month and r.week
+    uint8_t w = r.week;
 
-  if (w == 0)          // is this a "Last week" rule?
-  {
-    if (++m > 12)      // yes, for "Last", go to the next month
+    if (w == 0)              // is this a "Last week" rule?
     {
-      m = 1;
-      ++yr;
+        if (++m > 12)        // yes, for "Last", go to the next month
+        {
+            m = 1;
+            ++yr;
+        }
+        w = 1;               // and treat as first week of next month, subtract 7 days later
     }
-    w = 1; // and treat as first week of next month, subtract 7 days later
-  }
 
-  // calculate first day of the month, or for "Last" rules, first day of the next month
+    // calculate first day of the month, or for "Last" rules, first day of the next month
   struct tm tm;
   tm.tm_hour = r.hour;
   tm.tm_min  = 0;
@@ -111,14 +119,14 @@ uint32_t ESPEasy_time_zone::calcTimeChangeForRule(const TimeChangeRule& r, int y
   tm.tm_mday = 1;
   tm.tm_mon  = m;
   tm.tm_year = yr - 1970;
-  uint32_t t = makeTime(tm);
+    uint32_t t = makeTime(tm);
 
-  // add offset from the first of the month to r.dow, and offset for the given week
+    // add offset from the first of the month to r.dow, and offset for the given week
   t += ((r.dow - ESPEasy_time::weekday(t) + 7) % 7 + (w - 1) * 7) * SECS_PER_DAY;
 
-  // back up a week if this is a "Last" rule
+    // back up a week if this is a "Last" rule
   if (r.week == 0) { t -= 7 * SECS_PER_DAY; }
-  return t;
+    return t;
 }
 
 /*----------------------------------------------------------------------*
@@ -127,9 +135,9 @@ uint32_t ESPEasy_time_zone::calcTimeChangeForRule(const TimeChangeRule& r, int y
 *----------------------------------------------------------------------*/
 bool ESPEasy_time_zone::calcTimeChanges(int yr)
 {
-  uint32_t dstLoc  = calcTimeChangeForRule(m_dst, yr);
-  uint32_t stdLoc  = calcTimeChangeForRule(m_std, yr);
-  bool     changed = (m_dstLoc != dstLoc) || (m_stdLoc != stdLoc);
+  uint32_t dstLoc = calcTimeChangeForRule(m_dst, yr);
+  uint32_t stdLoc = calcTimeChangeForRule(m_std, yr);
+  bool changed = (m_dstLoc != dstLoc) || (m_stdLoc != stdLoc);
 
   m_dstLoc = dstLoc;
   m_stdLoc = stdLoc;
@@ -144,14 +152,14 @@ bool ESPEasy_time_zone::calcTimeChanges(int yr)
 *----------------------------------------------------------------------*/
 uint32_t ESPEasy_time_zone::toLocal(uint32_t utc)
 {
-  // recalculate the time change points if needed
+    // recalculate the time change points if needed
   if (ESPEasy_time::year(utc) != ESPEasy_time::year(m_dstUTC)) { calcTimeChanges(ESPEasy_time::year(utc)); }
 
   if (utcIsDST(utc)) {
-    return utc + m_dst.offset * SECS_PER_MIN;
+        return utc + m_dst.offset * SECS_PER_MIN;
   }
   else {
-    return utc + m_std.offset * SECS_PER_MIN;
+        return utc + m_std.offset * SECS_PER_MIN;
   }
 }
 
@@ -161,17 +169,17 @@ uint32_t ESPEasy_time_zone::toLocal(uint32_t utc)
 *----------------------------------------------------------------------*/
 bool ESPEasy_time_zone::utcIsDST(uint32_t utc)
 {
-  // recalculate the time change points if needed
+    // recalculate the time change points if needed
   if (ESPEasy_time::year(utc) != ESPEasy_time::year(m_dstUTC)) { calcTimeChanges(ESPEasy_time::year(utc)); }
 
   if (m_stdUTC == m_dstUTC) {     // daylight time not observed in this tz
-    return false;
+        return false;
   }
   else if (m_stdUTC > m_dstUTC) { // northern hemisphere
     return utc >= m_dstUTC && utc < m_stdUTC;
   }
   else {                          // southern hemisphere
-    return !(utc >= m_stdUTC && utc < m_dstUTC);
+        return !(utc >= m_stdUTC && utc < m_dstUTC);
   }
 }
 
@@ -181,17 +189,17 @@ bool ESPEasy_time_zone::utcIsDST(uint32_t utc)
 *----------------------------------------------------------------------*/
 bool ESPEasy_time_zone::locIsDST(uint32_t local)
 {
-  // recalculate the time change points if needed
+    // recalculate the time change points if needed
   if (ESPEasy_time::year(local) != ESPEasy_time::year(m_dstLoc)) { calcTimeChanges(ESPEasy_time::year(local)); }
 
   if (m_stdUTC == m_dstUTC) {     // daylight time not observed in this tz
-    return false;
+        return false;
   }
   else if (m_stdLoc > m_dstLoc) { // northern hemisphere
     return local >= m_dstLoc && local < m_stdLoc;
   }
   else {                          // southern hemisphere
-    return !(local >= m_stdLoc && local < m_dstLoc);
+        return !(local >= m_stdLoc && local < m_dstLoc);
   }
 }
 
