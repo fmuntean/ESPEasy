9961824419cde7791d48d2903e0847baf5844245
fmuntean <fmuntean@gmail.com>
2021-02-27 14:14:46 -0500

    adding CORE_DEBUG_LEVEL

--- src/src/ESPEasyCore/ESPEasy_Log.cpp
+++ src/src/ESPEasyCore/ESPEasy_Log.cpp
@@ -17,6 +17,11 @@
 #include "../Helpers/ESPEasy_Storage.h"
 #endif
 
+//MFD: include ESP debugging
+#ifdef CORE_DEBUG_LEVEL
+  #include <esp_log.h>
+#endif
+
 /********************************************************************************************\
   Init critical variables for logging (important during initial factory reset stuff )
   \*********************************************************************************************/
@@ -29,6 +34,11 @@ void initLog()
   setLogLevelFor(LOG_TO_SERIAL, 2); //logging during initialisation
   setLogLevelFor(LOG_TO_WEBLOG, 2);
   setLogLevelFor(LOG_TO_SDCARD, 0);
+
+  #ifdef CORE_DEBUG_LEVEL
+    esp_log_level_set("*", ESP_LOG_DEBUG);
+    log_i("enabled ESP logging!");
+  #endif
 }
 
 
